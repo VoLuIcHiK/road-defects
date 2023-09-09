da0c29457600418f78babb8848bc9439b5a74b5c
borntowarn <kozlovmihail02@mail.ru>
2023-09-10 01:09:11 +0300

    Added sqlite interraction
    Added taking every nth frame

--- src/db3_messages.py
+++ src/db3_messages.py
@@ -1,16 +1,18 @@
+import time
+import sqlite3
 import ros2_numpy
-from sensor_msgs.msg import PointCloud2
 import open3d as o3d
 
-import sqlite3
+
 from rosidl_runtime_py.utilities import get_message
 from rclpy.serialization import deserialize_message
 
 class BagFileParser():
-    def __init__(self, bag_file):
+    def __init__(self, bag_file, every_n_frame):
         self.conn = sqlite3.connect(bag_file)
         self.cursor = self.conn.cursor()
-
+        self.every_n_frame = every_n_frame
+        
         ## create a message type map
         topics_data = self.cursor.execute("SELECT id, name, type FROM topics").fetchall()
         self.topic_type = {name_of:type_of for id_of,name_of,type_of in topics_data}
@@ -24,42 +26,47 @@ class BagFileParser():
     def get_messages(self, topic_name):
         
         topic_id = self.topic_id[topic_name]
+        
+        query = "SELECT timestamp, data FROM messages WHERE topic_id = {} AND ROWID % {} = 0 ORDER BY timestamp"
+        
         # Get from the db
-        self.cursor.execute("SELECT timestamp, data FROM messages WHERE topic_id = {} ORDER BY timestamp".format(topic_id))
+        self.cursor.execute(query.format(topic_id, self.every_n_frame))
+
         # rows = self.cursor.execute("PRAGMA table_info(messages)").fetchall()
         # Deserialise all and timestamp them
         while True:
             row = self.cursor.fetchmany(1)
             if len(row):
-                timestamp, data = row[0]
-                yield (timestamp, deserialize_message(data, self.topic_msg_message[topic_name]))
+                for timestamp, data in row:
+                    data = deserialize_message(data, self.topic_msg_message[topic_name])
+                    yield (timestamp, data)
+                    
             else:
                 break
 
-class PointCloud2Converter:
-    def __init__(self):    
-        self.pc_npy = None
-        self.pcd = o3d.geometry.PointCloud()
+class PointCloudConverter:
 
-    def callback_pcl(self, messages):
+    @staticmethod
+    def write2pcd(messages):
         for mess in messages:
-            self.pc_npy  = ros2_numpy.point_cloud2.point_cloud2_to_array(mess)['xyz']
-            self.pcd.points = o3d.utility.Vector3dVector(self.pc_npy)
-            o3d.io.write_point_cloud("points_all.pcd", self.pcd)
+            pcd = o3d.geometry.PointCloud()
+            pc_npy  = ros2_numpy.point_cloud2.point_cloud2_to_array(mess)['xyz']
+            pcd.points = o3d.utility.Vector3dVector(pc_npy)
+            o3d.io.write_point_cloud("points_all.pcd", pcd)
+    
+    @staticmethod
+    def msg2cloud(msg):
+        pc_npy  = ros2_numpy.point_cloud2.point_cloud2_to_array(msg)['xyz']
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(pc_npy)
+        return pcd
 
 if __name__ == "__main__":
-    
     bag_file = '/mnt/c/Hack/Dataset/1/rosbag2_2023_09_04-11_56_58_0.db3'
 
-    parser = BagFileParser(bag_file)
-
-    import time
+    parser = BagFileParser(bag_file, 5)
     
     t1 = time.time()
-    
     for i, (timestamp, msg) in enumerate(parser.get_messages("/points")):
         pass
-    
-    print(time.time() - t1)
-    # conv = PointCloud2Converter()
-    # conv.callback_pcl(messages)
+    print(i, time.time() - t1)
